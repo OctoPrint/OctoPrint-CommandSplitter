31a43761edf0cfa00d0b1790ccff078de58a6230
Gina Häußge <gina@octoprint.org>
2020-04-24 18:50:59 +0200

    Make py3 compatible
    Closes #1

--- octoprint_commandsplitter/__init__.py
+++ octoprint_commandsplitter/__init__.py
@@ -7,38 +7,38 @@ import octoprint.plugin
 
 class CommandSplitterStream(octoprint.filemanager.util.LineProcessorStream):
 
-	comment_split_re = re.compile("(?<!\\\\);")
-	command_split_re = re.compile("(?<!\\\\):")
+	comment_split_re = re.compile(b"(?<!\\\\);")
+	command_split_re = re.compile(b"(?<!\\\\):")
 
 	def process_line(self, line):
 		line, comment = self.split_comment(line.strip())
-		if line == "" and len(comment):
-			return comment + "\n"
+		if line == b"" and len(comment):
+			return comment + b"\n"
 
 		lines = self.split_line(line)
 		if len(lines) == 0:
 			return None
 
-		return "\n".join(filter(lambda x: x is not None and x != "", lines)) + comment + "\n"
+		return b"\n".join(filter(lambda x: x is not None and x != b"", lines)) + comment + b"\n"
 
 	def split_comment(self, line):
-		if line.startswith(";"):
-			return "", line
+		if line.startswith(b";"):
+			return b"", line
 
-		if not ";" in line:
-			return line, ""
+		if not b";" in line:
+			return line, b""
 
 		line, comment = self.__class__.comment_split_re.split(line, maxsplit=1)
-		if comment != "":
-			comment = ";" + comment
+		if comment != b"":
+			comment = b";" + comment
 
 		return line, comment
 
 	def split_line(self, line):
-		if line == "":
+		if line == b"":
 			return []
 
-		if not ":" in line:
+		if not b":" in line:
 			return [line]
 
 		return map(lambda x: x.strip(), self.__class__.command_split_re.split(line.strip()))
@@ -51,6 +51,7 @@ def split_all_commands(path, file_object, links=None, printer_profile=None, allo
 
 __plugin_name__ = "CommandSplitter"
 __plugin_description__ = "Splits multiple commands on one GCODE line with \":\" separator into multiple lines."
+__plugin_pythoncompat__ = ">=2.7,<4"
 __plugin_hooks__ = {
 	"octoprint.filemanager.preprocessor": split_all_commands
 }
