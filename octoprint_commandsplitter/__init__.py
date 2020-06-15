4650fbb5568374836db26f83e26cdf8f91ec6e72
Gina Häußge <gina@octoprint.org>
2020-06-15 11:33:02 +0200

    Add software update hook

--- octoprint_commandsplitter/__init__.py
+++ octoprint_commandsplitter/__init__.py
@@ -1,47 +1,67 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-def process_line(line):
-	if b':' not in line:
-		# no :, bail
-		return line
+import octoprint
+import octoprint.plugin
 
-	# extract comments
-	comment = b''
-	if b';' in line:
-		line, comment = line.split(b';', maxsplit=1)
-		if line == b'' and len(comment):
-			# comment is the only thing on the line, bail
-			return comment + b'\n'
+class CommandSplitterStream(octoprint.filemanager.util.LineProcessorStream):
+	def process_line(self, line):
+		if b':' not in line:
+			# no :, bail
+			return line
 
-	result = b''
-	for l in line.split(b':'):
-		result += l.lstrip()
-		if l.endswith(b'\\'):
-			# escaped :, add back
-			result += b':'
-		else:
-			result = result.rstrip() + b'\n'
+		# extract comments
+		comment = b''
+		if b';' in line:
+			line, comment = line.split(b';', maxsplit=1)
+			if line == b'' and len(comment):
+				# comment is the only thing on the line, bail
+				return comment + b'\n'
 
-	if comment:
-		result = result[:-1] + b' ;' + comment
+		result = b''
+		for l in line.split(b':'):
+			result += l.lstrip()
+			if l.endswith(b'\\'):
+				# escaped :, add back
+				result += b':'
+			else:
+				result = result.rstrip() + b'\n'
 
-	return result.rstrip() + b'\n'
+		if comment:
+			result = result[:-1] + b' ;' + comment
 
-def split_all_commands(path, file_object, links=None, printer_profile=None, allow_overwrite=True, *args, **kwargs):
-	import octoprint
-	class CommandSplitterStream(octoprint.filemanager.util.LineProcessorStream):
-		def process_line(self, line):
-			return process_line(line)
+		return result.rstrip() + b'\n'
 
-	if not octoprint.filemanager.valid_file_type(path, type="gcode"):
-		return file_object
+class CommandSplitterPlugin(octoprint.plugin.OctoPrintPlugin):
 
-	return octoprint.filemanager.util.StreamWrapper(file_object.filename, CommandSplitterStream(file_object.stream()))
+	def split_all_commands(self, path, file_object, links=None, printer_profile=None, allow_overwrite=True, *args, **kwargs):
+		if not octoprint.filemanager.valid_file_type(path, type="gcode"):
+			return file_object
+
+		return octoprint.filemanager.util.StreamWrapper(file_object.filename, CommandSplitterStream(file_object.stream()))
+
+	def get_update_information(self, *args, **kwargs):
+		return dict(
+			commandsplitter=dict(
+				displayName="CommandSplitter Plugin",
+				displayVersion=self._plugin_version,
+
+				# version check: github repository
+				type="github_release",
+				user="OctoPrint",
+				repo="OctoPrint-CommandSplitter",
+				current=self._plugin_version,
+
+				# update method: pip
+				pip="https://github.com/OctoPrint/OctoPrint-CommandSplitter/archive/{target_version}.zip"
+			)
+		)
 
 __plugin_name__ = "CommandSplitter"
 __plugin_description__ = "Splits multiple commands on one GCODE line with \":\" separator into multiple lines."
 __plugin_pythoncompat__ = ">=2.7,<4"
+__plugin_implementation__ = CommandSplitterPlugin()
 __plugin_hooks__ = {
-	"octoprint.filemanager.preprocessor": split_all_commands
+	"octoprint.filemanager.preprocessor": __plugin_implementation__.split_all_commands,
+	"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
 }
